dec658069cd1b68fa7eedc9853b8fd14a7308f25
azilzor <athirsonarceus@gmail.com>
2023-03-28 15:49:52 -0300

    Initial commit

---
+++ src/main/java/api/wikimedia/WikimediaApplication.java
@@ -0,0 +1,13 @@
+package api.wikimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaApplication.class, args);
+	}
+
+}
